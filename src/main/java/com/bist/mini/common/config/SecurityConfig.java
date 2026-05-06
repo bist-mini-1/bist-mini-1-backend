c53de31e8aa69311a86b6be915b6db56daf45959
daehyuk1231 <daehyuk1231@naver.com>
2026-05-06 14:39:10 +0900

    feat: 로그인 API 및 JWT 발급 구현

--- src/main/java/com/bist/mini/common/config/SecurityConfig.java
+++ src/main/java/com/bist/mini/common/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.bist.mini.config;
+package com.bist.mini.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

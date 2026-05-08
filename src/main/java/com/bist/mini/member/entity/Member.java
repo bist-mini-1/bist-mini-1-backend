0b548553cdbf31647cd21580b3092bf75044604d
daehyuk1231 <daehyuk1231@naver.com>
2026-05-08 16:41:11 +0900

    feat: 로그인 컬럼에 맞게 수정

--- src/main/java/com/bist/mini/member/entity/Member.java
+++ src/main/java/com/bist/mini/member/entity/Member.java
@@ -12,7 +12,7 @@ public class Member {
     private String email;
     private String nickname;
     private String bio;
-    private String profileImage;
+    private byte[] profileImage;
     private String status;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
